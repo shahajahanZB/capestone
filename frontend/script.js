9524faabc9c409e89b3ee6edb13f711eb467021a
mh-Shaz <shahajahanbekinalkar@mhcognition.in>
2026-02-02 15:39:47 +0530

    deleted files

--- frontend/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const btn = document.querySelector('.btn');
-  btn.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#menu').scrollIntoView({behavior:'smooth'});
-  });
-});
